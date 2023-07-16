70cc8d32ee7c4c9cb509a850f5db3c869acbd60c
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2023-07-16 12:49:05 -0400

    metadata Head modification

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
-
+import Head from 'next/head'
 
 const FirstPost = () => {
   return (
     <div>
+      <Head>
+        <title>First title</title>
+      </Head>
       <h1>First Post</h1>
       <h2><Link href={"/"}>Home</Link></h2>
     </div>
