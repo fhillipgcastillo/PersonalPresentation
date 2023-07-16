38234523dd450e31f3b6b2f238b240d8431c85fe
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2023-07-16 12:52:35 -0400

    Adding third party scripts

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import Script from 'next/script'
+
 
 const FirstPost = () => {
   return (
@@ -8,6 +10,14 @@ const FirstPost = () => {
       <Head>
         <title>First title</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+
       <h1>First Post</h1>
       <h2><Link href={"/"}>Home</Link></h2>
     </div>
