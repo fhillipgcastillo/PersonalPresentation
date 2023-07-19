0d7c5d26cc046602042d3ca6dc3dc3dc22cdf05d
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2023-07-19 18:51:34 -0400

    * Updated posts lib to get placeholder posts  * Updated posts page to render new posts

--- lib/posts.ts
+++ lib/posts.ts
@@ -4,13 +4,14 @@ import matter from 'gray-matter';
 import { remark } from 'remark';
 import html from 'remark-html';
 
-
+const API_URL = 'https://jsonplaceholder.typicode.com';
 const postsDirectory = path.join(process.cwd(), 'posts');
 
-export interface PostData {
+export interface PostResponse {
+  userId: string;
   id: string;
   title: string;
-  date: string;
+  body: string;
 };
 
 export type PostParams = {
@@ -19,39 +20,20 @@ export type PostParams = {
   }
 };
 
-export interface PostHtmlData extends PostData {
-  contentHtml: string;
-};
+// export interface PostHtmlData extends PostData {
+//   contentHtml: string;
+// };
 
 
-export function getSortedPostsData(): PostData[] {
-  // Get file names under /posts
-  const fileNames: string[] = fs.readdirSync(postsDirectory);
-  const allPostsData: PostData[] = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, '');
+export async function getAllPosts(): Promise<PostResponse[]> {
+  const posts = await fetch(`${API_URL}/posts`)
+    .then((res) => res.json())
+    .then((data) => {
+      return data as PostResponse[];
+    });
 
-    // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, 'utf8');
-
-    // Use gray-matter to parse the post metadata section
-    const matterResult: matter.GrayMatterFile<string> = matter(fileContents);
-
-    // Combine the data with the id
-    return {
-      id,
-      ...(matterResult.data as { date: string; title: string; }),
-    };
-  });
   // Sort posts by date
-  return allPostsData.sort((a, b) => {
-    if (a.date < b.date) {
-      return 1;
-    } else {
-      return -1;
-    }
-  });
+  return posts;
 }
 
 /**
@@ -71,23 +53,6 @@ export function getAllPostIds(): PostParams[] {
 };
 
 
-export async function getPostData(id: string): Promise<PostHtmlData> {
-  const fullPath = path.join(postsDirectory, `${id}.md`);
-  const fileContents = fs.readFileSync(fullPath, 'utf8');
-
-  // Use gray-matter to parse the post metadata section
-  const matterResult = matter(fileContents);
-
-  // Use remark to convert markdown into HTML string
-  const processedContent = await remark()
-    .use(html)
-    .process(matterResult.content);
-  const contentHtml = processedContent.toString();
-
-  // Combine the data with the id and contentHtml
-  return {
-    id,
-    contentHtml,
-    ...matterResult.data,
-  } as PostHtmlData;
-}
+// export async function getPostData(id: string): Promise<PostResponse> {
+  
+// }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,16 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/Layout';
 import utilStyles from '../styles/utils.module.css';
-import { PostData, getSortedPostsData } from '../lib/posts';
+import { PostResponse, getAllPosts } from '../lib/posts';
 import Link from 'next/link';
-import Date from '../components/Date';
 import { ReactElement } from 'react';
 import { GetStaticProps, GetStaticPropsResult } from 'next';
 
 interface Props {
-  allPostsData: PostData[];
+  posts: PostResponse[];
 }
 
-export default function Home({allPostsData}:Props):ReactElement {
+export default function Home({ posts }: Props): ReactElement {
   return (
     <Layout home>
       <Head>
@@ -20,26 +19,27 @@ export default function Home({allPostsData}:Props):ReactElement {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>{title}</Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
-          ))}
+          {posts
+            .map(({ id, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>{title}</Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  User:
+                </small>
+              </li>
+            ))}
         </ul>
       </section>
     </Layout>
   );
 }
 
-export const getStaticProps:GetStaticProps = async (/*{ props, preview, params }*/):Promise<GetStaticPropsResult<Props>> => {
-  const allPostsData:PostData[] = getSortedPostsData();
+export const getStaticProps: GetStaticProps = async (/*{ props, preview, params }*/): Promise<GetStaticPropsResult<Props>> => {
+  const posts: PostResponse[] = await getAllPosts();
   return {
     props: {
-      allPostsData
+      posts
     }
   }
 }
