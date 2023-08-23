7ef4528d94e6b168e20462d074a28ab56b8afd17
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2023-08-23 17:18:27 -0400

    Remove previously used  API_URL

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,13 +1,10 @@
-import Date from '../../components/Date';
 import Layout from '../../components/Layout';
-import { API_URL, PostResponse, getAllPostIdsPaths, getFullPostData, getPostData } from '../../lib/posts';
+import { getAllPostIdsPaths, getFullPostData } from '../../lib/posts';
 import Head from 'next/head';
 
 import utilStyles from '../../styles/utils.module.css';
 import React from 'react';
-import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
-import { fetcher } from '../../lib/utils';
-import { getUserById } from '../../lib/users';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import postsStyles from '../../components/PostPreviewItem/PostPreviewItem.module.css';
 
 export default function Post({ postData }): React.ReactElement {
@@ -30,7 +27,7 @@ export default function Post({ postData }): React.ReactElement {
                 {postData?.comments?.data.map((comment) => (
                     <li key={comment.id} className={postsStyles.commentItem}>
                         <p className={postsStyles.commentUser}>{comment.email}</p>
-                        <div style={{padding: "0 15px"}}>
+                        <div style={{ padding: "0 15px" }}>
                             <p>{comment.name}</p>
                             <p>{comment.body}</p>
                         </div>
@@ -53,7 +50,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async ({ params, preview }) => {
     const postData = await getFullPostData(Number(params?.id));
-    
+
     return {
         props: {
             postData
