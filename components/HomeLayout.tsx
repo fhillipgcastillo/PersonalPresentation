ad92eab605cccfa6d36aa5e90787ff8bc6eb74aa
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2023-11-19 16:19:21 -0400

    Renamed PageContent to PostPreview

--- components/HomeLayout.tsx
+++ components/HomeLayout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from './Layout'
 import { Button, Grid, Typography } from '@mui/material'
-import PageContent from './PageContent'
+import PostPreview from './PostPreview'
 import { PostsPaginated } from '../lib/graphqlQuery'
 
 export interface HomeLayoutProps {
@@ -32,7 +32,7 @@ const HomeLayout = (props: HomeLayoutProps) => {
                 </Grid>
             </Grid>
             <Grid item container flexDirection="column" sx={{ padding: "10px" }}>
-                <PageContent postsData={postsData} />
+                <PostPreview postsData={postsData} />
             </Grid>
         </Layout>
     )
