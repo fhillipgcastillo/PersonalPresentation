79ee224d97f6871068013d721b261cd9aebcf0a2
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2023-11-26 16:28:44 -0400

    Move posts index

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,25 @@ import { ReactElement } from 'react';
 import { GetStaticProps } from 'next';
 import { PostsPaginated } from '../lib/dataTypes';
 import HomeLayout, { HomeLayoutProps } from '../components/HomeLayout';
+import Layout from '../components/Layout';
 
-
-export default HomeLayout;
-
-export const getStaticProps: GetStaticProps<HomeLayoutProps> = async ({ preview, params }) => {
-
-  const data: { posts: PostsPaginated } = await getPostsPaginated();
-  return {
-    props: {
-      postsData: data.posts,
-    }
-  }
+const Home = () => {
+  return (
+    <Layout home siteTitle='Fhillip Castillo'>
+    </Layout>
+  )
 }
+export default Home;
+
+// export const getStaticProps: GetStaticProps<HomeLayoutProps> = async ({ preview, params }) => {
+
+//   const data: { posts: PostsPaginated } = await getPostsPaginated();
+//   return {
+//     props: {
+//       postsData: data.posts,
+//     }
+//   }
+// }
 
 /**
  * This is an example if we want to use serside rendering
---
+++ pages/posts/index.tsx
@@ -0,0 +1,29 @@
+import { getPostsPaginated } from '../../lib/posts';
+import { ReactElement } from 'react';
+import { GetStaticProps } from 'next';
+import { PostsPaginated } from '../../lib/dataTypes';
+import HomeLayout, { HomeLayoutProps } from '../../components/HomeLayout';
+
+
+export default HomeLayout;
+
+export const getStaticProps: GetStaticProps<HomeLayoutProps> = async ({ preview, params }) => {
+
+  const data: { posts: PostsPaginated } = await getPostsPaginated();
+  return {
+    props: {
+      postsData: data.posts,
+    }
+  }
+}
+
+/**
+ * This is an example if we want to use serside rendering
+ * export const getServerSideProps: GetServerSideProps = async (context){
+    return {
+      props: {
+        // props for your component
+      },
+    };
+  }
+ */
